5c300108c2b1ae9bf5f41cc07275dc893bdc942b
Tibi <tyby_tiberiu@yahoo.com>
2024-10-04 01:58:55 +0300

    Create Next.js app, install dependecies, set folder structure, add boilerplate and adapt it to Next.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+import Layout from "@/components/layout/Layout";
+import Home from "@/components/Home";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Index() {
+  return (
+    <Layout>
+
+      <Home/>
+
+    </Layout>
+  );
+}
