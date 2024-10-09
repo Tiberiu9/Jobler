ab0754c40a7afeb33b10a42397c74af36c8e532f
Tibi <tyby_tiberiu@yahoo.com>
2024-10-09 22:43:29 +0300

    Add job search functionality

---
+++ frontend/pages/search.js
@@ -0,0 +1,16 @@
+import Layout from "@/components/layout/Layout";
+import Search from "@/components/layout/Search";
+
+
+export default function SearchPage() {
+
+  return (
+    <Layout title="Search your job">
+
+      <Search/>
+
+    </Layout>
+  );
+}
+
+
