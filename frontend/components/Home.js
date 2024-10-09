3ad1207b92eda9bffbf8780f99c62abf987c51ef
Tibi <tyby_tiberiu@yahoo.com>
2024-10-10 00:19:30 +0300

    Add pagination

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -3,12 +3,32 @@ import Link from "next/link";
 import JobItem from "@/components/job/JobItem";
 import {useRouter} from "next/router";
 
-const Home = ({ data }) => {
+import Pagination from 'react-js-pagination';
+
+const Home = ({data}) => {
 
   const {jobs, count, resPerPage} = data
 
   const router = useRouter()
-  const {keyword} = router.query
+  let {page = 1, keyword} = router.query
+  page = Number(page)
+
+  let queryParams;
+  if (typeof window !== "undefined") {
+    queryParams = new URLSearchParams(window.location.search);
+  }
+
+
+  const handlePageClick = (currentPage) => {
+    if (queryParams.has("page")) {
+      queryParams.set("page", currentPage);
+    } else {
+      queryParams.append("page", currentPage);
+    }
+    router.push({
+      search: queryParams.toString(),
+    });
+  };
 
   return (
     <div className="container container-fluid">
@@ -32,6 +52,23 @@ const Home = ({ data }) => {
             </div>
           </div>
           {jobs && jobs.map((job) => <JobItem key={job.id} job={job}/>)}
+
+          {resPerPage < count && (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={page}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={count}
+                onChange={handlePageClick}
+                nextPageText={"Next"}
+                prevPageText={"Prev"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass="page-item"  // page-item is the class name from the boilerplate
+                linkClass="page-link"
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
